124ca5cf5bb3b8df430ec9a5cd40c0dd3d4c3a45
tuthangphat <81029660+tuthangphat@users.noreply.github.com>
2021-08-17 21:15:02 +0700

    update

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
 // Code by Phat's Dev
-
-function hevaly(options){
-    options.class == '' || ""
-    options.padding == '' || ""
-    options.margin == '' || ""
-    options.background == '' || ""
-    options.shadow_type == '' || ""
-
-    const elements = document.querySelectorAll(options.class);
-    elements.forEach(element => {
-        if(options.shadow_type == options.shadow_type){
-            element.style.boxShadow = options.shadow_type;
-        }
-        if(options.padding === options.padding){
-            element.style.padding = options.padding;
-        }
-        if(options.margin === options.margin){
-            element.style.margin = options.margin;
-        }
-        if(options.background === options.background){
-            element.style.background = options.background;
-        }
-    });
-}
-
+import { hevaly } from './module/hevaly'
 
 module.exports.hevaly = hevaly;
---
+++ module/hevaly.js
@@ -0,0 +1,28 @@
+function hevaly(options){
+    options.class == '' || ""
+    options.padding == '' || ""
+    options.margin == '' || ""
+    options.background == '' || ""
+    options.shadow_type == '' || ""
+    options.borderRadius == '' | ""
+
+    const elements = document.querySelectorAll(options.class);
+    const elementss = document.getElementById("test")
+    elements.forEach(element => {
+        if(options.shadow_type == options.shadow_type){
+            element.style.boxShadow = options.shadow_type;
+        }
+        if(options.padding === options.padding){
+            element.style.padding = options.padding;
+        }
+        if(options.margin === options.margin){
+            element.style.margin = options.margin;
+        }
+        if(options.background === options.background){
+            element.style.background = options.background;
+        }
+        if(options.borderRadius === options.borderRadius){
+            element.borderRadius = options.borderRadius
+        }
+    });
+}
